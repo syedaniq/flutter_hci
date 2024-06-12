f138bc50855740e461e519b44fa1f4edaeab1973
Aniq Hussain Syed <syedaniq@outlook.de>
2024-06-12 10:44:41 +0200

    convert stateful to stateless
    unnecessary stateful widget use

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,14 +6,9 @@ import '../providers/studentlist_provider.dart';
 import 'create_container.dart';
 import '../widgets/mein_container.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Consumer<StudentsList>(
